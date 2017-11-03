837100909bd9586490209512183a6510db1b39f7
supachok654 <dome654@hotmail.co.th>
2017-11-03 15:50:53 +0700

    left right control

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,5 +1,6 @@
 package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -28,6 +29,11 @@ public class GameScreen extends ScreenAdapter {
         batch.end();
     }
     private void update(float delta) {
-    	x += 5;
+    	if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+            x -= 10;
+        }
+        if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+            x += 10;
+        }
     }
 }
